3b89d92044a230bf33b0857bc910ccc2138258d4
Alifernu <35994389+Alifernu@users.noreply.github.com>
2018-10-04 17:44:03 +0200

    Fonctionnel
    A faire : Utiliser fichier json

--- iframe_qwant/javascript/index.js
+++ iframe_qwant/javascript/index.js
@@ -2,45 +2,54 @@
 ======================================*/
 var playlist = [
 	{
-		"song"    : "House of the Rising Sun",
-		"album"   : "The Animals",
-		"artist"  : "The Animals",
-		"artwork" : "http://upload.wikimedia.org/wikipedia/en/thumb/a/a8/Rising_sun_animals_US.jpg/220px-Rising_sun_animals_US.jpg",
-		"mp3"     : "http://retro-disko.ru/6/music/016.mp3"
+		"name"    : "House of the Rising Sun",
+		"album_name"   : "The Animals",
+		"artist_name"  : "The Animals",
+		"album_picture" : "http://upload.wikimedia.org/wikipedia/en/thumb/a/a8/Rising_sun_animals_US.jpg/220px-Rising_sun_animals_US.jpg",
+		"preview_url"     : "http://retro-disko.ru/6/music/016.mp3"
 	},
 	{
-		"song"    : "Superstition",
-		"album"   : "Talking Book",
-		"artist"  : "Stevie Wonder",
-		"artwork" : "https://i.imgur.com/Py4XcBT.png",
-		"mp3"     : "http://vocaroo.com/media_command.php?media=s1WYNvqulYH9&command=download_mp3"
+		"name"    : "Superstition",
+		"album_name"   : "Talking Book",
+		"artist_name"  : "Stevie Wonder",
+		"album_picture" : "https://i.imgur.com/Py4XcBT.png",
+		"preview_url"     : "http://vocaroo.com/media_command.php?media=s1WYNvqulYH9&command=download_mp3"
 	},
 	{
-		"song"    : "I Need You Back",
-		"album"   : "Premiere",
-		"artist"  : "The Noisy Freaks",
-		"artwork" : "http://i1285.photobucket.com/albums/a583/TheGreatOzz1/Hosted-Images/Noisy-Freeks-Image_zps4kilrxml.png",
-		"mp3"     : "http://kirkbyo.net/Assets/The-Noisy-Freaks.mp3"
+		"name"    : "I Need You Back",
+		"album_name"   : "Premiere",
+		"artist_name"  : "The Noisy Freaks",
+		"album_picture" : "http://i1285.photobucket.com/albums/a583/TheGreatOzz1/Hosted-Images/Noisy-Freeks-Image_zps4kilrxml.png",
+		"preview_url"     : "http://kirkbyo.net/Assets/The-Noisy-Freaks.mp3"
+	},
+	// {
+	// 	"song"    : "Yeux disent",
+	// 	"album"   : "FLIP (Deluxe)",
+	// 	"artist"  : "Lomepal",
+	// 	"artwork" : "https://s2.qwant.com/thumbr/0x0/d/f/cea0917cf81d6b364fc76cd545bf5954ff2348b7148cb87893401114b0db76/170x170bb.jpg?u=https%3A%2F%2Fis1-ssl.mzstatic.com%2Fimage%2Fthumb%2FMusic118%2Fv4%2F9a%2Ff6%2F4b%2F9af64be7-6734-5526-21c1-cb3a294526b8%2F3663729044303_cover.jpg%2F170x170bb.jpg&q=0&b=1&p=0&a=0",
+	// 	"mp3"	  : "https://audio-ssl.itunes.apple.com/apple-assets-us-std-000001/AudioPreview128/v4/69/5a/f0/695af0ed-7814-6dd5-a977-1853c3f849cd/mzaf_6527844017270148074.plus.aac.p.m4a"
+	// },
+	{
+		"name"    		: "Yeux disent",
+		"album_name"    : "FLIP (Deluxe)",
+		"artist_name"   : "Lomepal",
+		"album_picture" : "https://s2.qwant.com/thumbr/0x0/d/f/cea0917cf81d6b364fc76cd545bf5954ff2348b7148cb87893401114b0db76/170x170bb.jpg?u=https%3A%2F%2Fis1-ssl.mzstatic.com%2Fimage%2Fthumb%2FMusic118%2Fv4%2F9a%2Ff6%2F4b%2F9af64be7-6734-5526-21c1-cb3a294526b8%2F3663729044303_cover.jpg%2F170x170bb.jpg&q=0&b=1&p=0&a=0",
+		"preview_url"	: "https://audio-ssl.itunes.apple.com/apple-assets-us-std-000001/AudioPreview128/v4/69/5a/f0/695af0ed-7814-6dd5-a977-1853c3f849cd/mzaf_6527844017270148074.plus.aac.p.m4a"
 	}
 ];
 
 /* General Load / Variables
 ======================================*/
-var rot = 0;
 var duration;
 var playPercent;
-var rotate_timer;
-var armrot = -45;
 var bufferPercent;
 var currentSong = 0;
-var arm_rotate_timer;
-var arm = document.getElementById("arm");
 var next = document.getElementById("next");
-var song = document.getElementById("song");
+var song = document.getElementById("name");
 var timer = document.getElementById("timer");
 var music = document.getElementById("music");
-var album = document.getElementById("album");
-var artist = document.getElementById("artist");
+var album = document.getElementById("album_name");
+var artist = document.getElementById("artist_name");
 var volume = document.getElementById("volume");
 var playButton = document.getElementById("play");
 var timeline = document.getElementById("slider");
@@ -48,7 +57,7 @@ var playhead = document.getElementById("elapsed");
 var previous = document.getElementById("previous");
 var pauseButton = document.getElementById("pause");
 var bufferhead = document.getElementById("buffered");
-var artwork = document.getElementsByClassName("artwork")[0];
+var artwork = document.getElementsByClassName("album_picture")[0];
 var timelineWidth = timeline.offsetWidth - playhead.offsetWidth;
 var visablevolume = document.getElementsByClassName("volume")[0];
 
@@ -61,38 +70,31 @@ load();
 ======================================*/
 function load(){
 	pauseButton.style.visibility = "hidden";
-	song.innerHTML = playlist[currentSong]['song'];
-	song.title = playlist[currentSong]['song'];
-	album.innerHTML = playlist[currentSong]['album'];
-	album.title = playlist[currentSong]['album'];
-	artist.innerHTML = playlist[currentSong]['artist'];
-	artist.title = playlist[currentSong]['artist'];
-	artwork.setAttribute("style", "background:url(https://i.imgur.com/3idGgyU.png), url('"+playlist[currentSong]['artwork']+"') center no-repeat;");
-	music.innerHTML = '<source src="'+playlist[currentSong]['mp3']+'" type="audio/mp3">';
+	song.innerHTML = playlist[currentSong]['name'];
+	song.title = playlist[currentSong]['name'];
+	album.innerHTML = playlist[currentSong]['album_name'];
+	album.title = playlist[currentSong]['album_name'];
+	artist.innerHTML = playlist[currentSong]['artist_name'];
+	artist.title = playlist[currentSong]['artist_name'];
+	artwork.setAttribute("style", "background:url('"+playlist[currentSong]['album_picture']+"'), center no-repeat;");
+	music.innerHTML = '<source src="'+playlist[currentSong]['preview_url']+'" type="audio/mp3">';
 	music.load();
 }
 function reset(){ 
-	rotate_reset = setInterval(function(){ 
-		Rotate();
-		if(rot == 0){
-			clearTimeout(rotate_reset);
-		}
-	}, 1);
 	fireEvent(pauseButton, 'click');
-	armrot = -45;
 	playhead.style.width = "0px";
 	bufferhead.style.width = "0px";
 	timer.innerHTML = "0:00";
 	music.innerHTML = "";
 	currentSong = 0; // set to first song, to stay on last song: currentSong = playlist.length - 1;
-	song.innerHTML = playlist[currentSong]['song'];
-	song.title = playlist[currentSong]['song'];
-	album.innerHTML = playlist[currentSong]['album'];
-	album.title = playlist[currentSong]['album'];
-	artist.innerHTML = playlist[currentSong]['artist'];
-	artist.title = playlist[currentSong]['artist'];
-	artwork.setAttribute("style", "background:url(https://i.imgur.com/3idGgyU.png), url('"+playlist[currentSong]['artwork']+"') center no-repeat;");
-	music.innerHTML = '<source src="'+playlist[currentSong]['mp3']+'" type="audio/mp3">';
+	song.innerHTML = playlist[currentSong]['name'];
+	song.title = playlist[currentSong]['name'];
+	album.innerHTML = playlist[currentSong]['album_name'];
+	album.title = playlist[currentSong]['album_name'];
+	artist.innerHTML = playlist[currentSong]['artist_name'];
+	artist.title = playlist[currentSong]['artist_name'];
+	artwork.setAttribute("style", "background:url('"+playlist[currentSong]['album_picture']+"'), center no-repeat;");
+	music.innerHTML = '<source src="'+playlist[currentSong]['preview_url']+'" type="audio/mp3">';
 	music.load();
 }
 function formatSecondsAsTime(secs, format) {
@@ -114,24 +116,7 @@ function bufferUpdate() {
 	bufferPercent = timelineWidth * (music.buffered.end(0) / duration);
 	bufferhead.style.width = bufferPercent + "px";
 }
-function Rotate(){
-	if(rot == 361){
-		artwork.style.transform = 'rotate(0deg)';
-		rot = 0;
-	} else {
-		artwork.style.transform = 'rotate('+rot+'deg)';
-		rot++;
-	}
-}
-function RotateArm(){
-	if(armrot > -12){
-		arm.style.transform = 'rotate(-38deg)';
-		armrot = -45;
-	} else {
-		arm.style.transform = 'rotate('+armrot+'deg)';
-		armrot = armrot + (26 / duration);
-	}
-}
+
 function fireEvent(el, etype){
 	if (el.fireEvent) {
 		el.fireEvent('on' + etype);
@@ -157,93 +142,54 @@ pauseButton.onclick = function() {
 music.addEventListener("play", function () {
 	playButton.style.visibility = "hidden";
 	pause.style.visibility = "visible";
-	rotate_timer = setInterval(function(){ 
-		if(!music.paused && !music.ended && 0 < music.currentTime){
-			Rotate();
-		}
-	}, 10);	
-	if(armrot != -45){
-		arm.setAttribute("style", "transition: transform 800ms;");
-		arm.style.transform = 'rotate('+armrot+'deg)';
-	}
-	arm_rotate_timer = setInterval(function(){ 
-		if(!music.paused && !music.ended && 0 < music.currentTime){
-			if(armrot == -45){
-				arm.setAttribute("style", "transition: transform 800ms;");
-				arm.style.transform = 'rotate(-38deg)';
-				armrot = -38;
-			}
-			if(arm.style.transition != ""){
-				setTimeout(function(){
-					arm.style.transition = "";
-				}, 1000);
-			}
-			RotateArm();
-		}
-	}, 1000);
 }, false);
 music.addEventListener("pause", function () {
-	arm.setAttribute("style", "transition: transform 800ms;");
-	arm.style.transform = 'rotate(-45deg)';
 	playButton.style.visibility = "visible";
 	pause.style.visibility = "hidden";
-	clearTimeout(rotate_timer);
-	clearTimeout(arm_rotate_timer);
 }, false);
+
 next.onclick = function(){
-	arm.setAttribute("style", "transition: transform 800ms;");
-	arm.style.transform = 'rotate(-45deg)';
-	clearTimeout(rotate_timer);
-	clearTimeout(arm_rotate_timer);
 	playhead.style.width = "0px";
 	bufferhead.style.width = "0px";
 	timer.innerHTML = "0:00";
 	music.innerHTML = "";
-	arm.style.transform = 'rotate(-45deg)';
-	armrot = -45;
 	if((currentSong + 1) == playlist.length){
 		currentSong = 0;
-		music.innerHTML = '<source src="'+playlist[currentSong]['mp3']+'" type="audio/mp3">';
+		music.innerHTML = '<source src="'+playlist[currentSong]['preview_url']+'" type="audio/mp3">';
 	} else {
 		currentSong++;
-		music.innerHTML = '<source src="'+playlist[currentSong]['mp3']+'" type="audio/mp3">';
+		music.innerHTML = '<source src="'+playlist[currentSong]['preview_url']+'" type="audio/mp3">';
 	}
-	song.innerHTML = playlist[currentSong]['song'];
-	song.title = playlist[currentSong]['song'];
-	album.innerHTML = playlist[currentSong]['album'];
-	album.title = playlist[currentSong]['album'];
-	artist.innerHTML = playlist[currentSong]['artist'];
-	artist.title = playlist[currentSong]['artist'];
-	artwork.setAttribute("style", "transform: rotate("+rot+"deg); background:url(https://i.imgur.com/3idGgyU.png), url('"+playlist[currentSong]['artwork']+"') center no-repeat;");
+	song.innerHTML = playlist[currentSong]['name'];
+	song.title = playlist[currentSong]['name'];
+	album.innerHTML = playlist[currentSong]['album_name'];
+	album.title = playlist[currentSong]['album_name'];
+	artist.innerHTML = playlist[currentSong]['artist_name'];
+	artist.title = playlist[currentSong]['artist_name'];
+	artwork.setAttribute("style", "background:url('"+playlist[currentSong]['album_picture']+"'), center no-repeat;");
 	music.load();
 	duration = music.duration;
 	music.play();
 }
 previous.onclick = function(){
-	arm.setAttribute("style", "transition: transform 800ms;");
-	arm.style.transform = 'rotate(-45deg)';
-	clearTimeout(rotate_timer);
-	clearTimeout(arm_rotate_timer);
 	playhead.style.width = "0px";
 	bufferhead.style.width = "0px";
 	timer.innerHTML = "0:00";
 	music.innerHTML = "";
-	arm.style.transform = 'rotate(-45deg)';
-	armrot = -45;
 	if((currentSong - 1) == -1){
 		currentSong = playlist.length - 1;
-		music.innerHTML = '<source src="'+playlist[currentSong]['mp3']+'" type="audio/mp3">';
+		music.innerHTML = '<source src="'+playlist[currentSong]['preview_url']+'" type="audio/mp3">';
 	} else {
 		currentSong--;
-		music.innerHTML = '<source src="'+playlist[currentSong]['mp3']+'" type="audio/mp3">';
+		music.innerHTML = '<source src="'+playlist[currentSong]['preview_url']+'" type="audio/mp3">';
 	}
-	song.innerHTML = playlist[currentSong]['song'];
-	song.title = playlist[currentSong]['song'];
-	album.innerHTML = playlist[currentSong]['album'];
-	album.title = playlist[currentSong]['album'];
-	artist.innerHTML = playlist[currentSong]['artist'];
-	artist.title = playlist[currentSong]['artist'];
-	artwork.setAttribute("style", "transform: rotate("+rot+"deg); background:url(https://i.imgur.com/3idGgyU.png), url('"+playlist[currentSong]['artwork']+"') center no-repeat;");
+	song.innerHTML = playlist[currentSong]['name'];
+	song.title = playlist[currentSong]['name'];
+	album.innerHTML = playlist[currentSong]['album_name'];
+	album.title = playlist[currentSong]['album_name'];
+	artist.innerHTML = playlist[currentSong]['artist_name'];
+	artist.title = playlist[currentSong]['artist_name'];
+	artwork.setAttribute("style", "background:url('"+playlist[currentSong]['album_picture']+"'), center no-repeat;");
 	music.load();
 	duration = music.duration;
 	music.play();
